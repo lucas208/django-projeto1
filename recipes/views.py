d21a808f99e5a8539966d193f9410369ef21deb1
Lucas Vieira <lucas.vieira.093@ufrn.edu.br>
2022-12-27 00:46:17 -0300

    Criando e renderizando primeiros templates

--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse('HOME')
+    return render(request, 'recipes/home.html', context={
+        'name': 'Lucas',
+    })
 
 
 def contato(request):
